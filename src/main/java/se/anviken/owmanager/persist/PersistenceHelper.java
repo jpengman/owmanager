50e349d6224e9d48d6ca17661242ab634ead124f
Jesper Engman <jpengman@gmail.com>
2018-03-15 09:33:58 +0100

    Created PersistanceHelper to unclutter DataEndpoint class

---
+++ src/main/java/se/anviken/owmanager/persist/PersistenceHelper.java
@@ -0,0 +1,171 @@
+package se.anviken.owmanager.persist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.google.visualization.datasource.base.TypeMismatchException;
+import com.google.visualization.datasource.datatable.ColumnDescription;
+import com.google.visualization.datasource.datatable.DataTable;
+import com.google.visualization.datasource.datatable.TableRow;
+import com.google.visualization.datasource.datatable.value.ValueType;
+import com.google.visualization.datasource.render.JsonRenderer;
+
+import se.anviken.owmanager.dto.TemperatureDTO;
+import se.anviken.owmanager.model.Sensor;
+import se.anviken.owmanager.model.Temperature;
+import se.anviken.owmanager.utils.TimeUtil;
+
+@Stateless
+public class PersistenceHelper {
+	@PersistenceContext(unitName = "OWManager-persistence-unit")
+	private EntityManager em;
+
+	protected List<Temperature> getDataSet(int id, int noofminutes) {
+		Calendar from = Calendar.getInstance();
+		Calendar to = Calendar.getInstance();
+		from.add(Calendar.MINUTE, -noofminutes);
+		TypedQuery<Temperature> tempQuery = em.createQuery(
+				"SELECT DISTINCT t FROM Temperature t LEFT JOIN FETCH t.sensor WHERE t.sensor.sensorId = :id AND t.tempTimestamp BETWEEN :from AND :to",
+				Temperature.class);
+		tempQuery.setParameter("from", from.getTime());
+		tempQuery.setParameter("to", to.getTime());
+		tempQuery.setParameter("id", id);
+		return tempQuery.getResultList();
+	}
+
+	protected Response getDatatableByType(String type, int noofminutes) {
+		String[] typeArray = type.split("\\s*,\\s*");
+		List<String> sensorTypes = Arrays.asList(typeArray);
+		TypedQuery<Sensor> query = em
+				.createQuery("SELECT s FROM Sensor s where s.sensorType.sensorType IN :sensorTypes", Sensor.class);
+		query.setParameter("sensorTypes", sensorTypes);
+		final List<Sensor> results = query.getResultList();
+		DataTable data = new DataTable();
+		ArrayList<ColumnDescription> cd = new ArrayList<ColumnDescription>();
+		cd.add(new ColumnDescription("Tid", ValueType.TEXT, "Tid"));
+
+		List<Map<Date, Float>> temperatureLists = new ArrayList<Map<Date, Float>>();
+		Set<Date> timestamps = new HashSet<Date>();
+
+		for (Sensor sensor : results) {
+			cd.add(new ColumnDescription(sensor.getName(), ValueType.NUMBER, sensor.getName()));
+			Map<Date, Float> temperarures = new HashMap<Date, Float>();
+			for (Temperature temperature : getDataSet(sensor.getSensorId(), noofminutes)) {
+				temperarures.put(temperature.getTempTimestamp(),
+						temperature.getTemperature() + temperature.getSensor().getOffset());
+				timestamps.add(temperature.getTempTimestamp());
+			}
+			temperatureLists.add(temperarures);
+		}
+		data.addColumns(cd);
+		timestamps = new TreeSet<Date>(timestamps);
+		for (Date timestamp : timestamps) {
+			TableRow row = new TableRow();
+			row.addCell(TimeUtil.getTimeOfDay(timestamp));
+			for (Map<Date, Float> temperatureList : temperatureLists) {
+				row.addCell(temperatureList.get(timestamp));
+			}
+			try {
+				data.addRow(row);
+			} catch (TypeMismatchException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return Response.ok(JsonRenderer.renderDataTable(data, true, false, true).toString(), MediaType.APPLICATION_JSON)
+				.build();
+	}
+
+	protected Response getDatatable(String ids, int noofminutes) {
+		String[] strings = ids.split("\\s*,\\s*");
+		Integer[] intarray = new Integer[strings.length];
+		int i = 0;
+		for (String str : strings) {
+			intarray[i] = Integer.parseInt(str.trim());
+			i++;
+		}
+
+		List<Integer> sensorIDs = Arrays.asList(intarray);
+		TypedQuery<Sensor> query = em.createQuery("SELECT s FROM Sensor s where s.sensorId IN :sensors", Sensor.class);
+		query.setParameter("sensors", sensorIDs);
+		final List<Sensor> results = query.getResultList();
+		DataTable data = new DataTable();
+		ArrayList<ColumnDescription> cd = new ArrayList<ColumnDescription>();
+		cd.add(new ColumnDescription("Tid", ValueType.TEXT, "Tid"));
+		for (Sensor sensor : results) {
+			cd.add(new ColumnDescription(sensor.getName(), ValueType.NUMBER, sensor.getName()));
+		}
+		data.addColumns(cd);
+		List<Map<Date, Float>> temperatureLists = new ArrayList<Map<Date, Float>>();
+		Set<Date> timestamps = new HashSet<Date>();
+
+		for (int id : sensorIDs) {
+			Map<Date, Float> temperarures = new HashMap<Date, Float>();
+			for (Temperature temperature : getDataSet(id, noofminutes)) {
+				temperarures.put(temperature.getTempTimestamp(),
+						temperature.getTemperature() + temperature.getSensor().getOffset());
+				timestamps.add(temperature.getTempTimestamp());
+			}
+			temperatureLists.add(temperarures);
+		}
+		timestamps = new TreeSet<Date>(timestamps);
+		for (Date timestamp : timestamps) {
+			TableRow row = new TableRow();
+			row.addCell(TimeUtil.getTimeOfDay(timestamp));
+			for (Map<Date, Float> temperatureList : temperatureLists) {
+				row.addCell(temperatureList.get(timestamp));
+			}
+			try {
+				data.addRow(row);
+			} catch (TypeMismatchException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return Response.ok(JsonRenderer.renderDataTable(data, true, false, true).toString(), MediaType.APPLICATION_JSON)
+				.build();
+	}
+
+	protected TemperatureDTO getTemperature(int id) {
+		TypedQuery<Sensor> findByIdQuery = em.createQuery(
+				"SELECT DISTINCT s FROM Sensor s LEFT JOIN FETCH s.sensorType WHERE s.sensorId = :entityId ORDER BY s.sensorId",
+				Sensor.class);
+		findByIdQuery.setParameter("entityId", id);
+		Sensor entity;
+		try {
+			entity = findByIdQuery.getSingleResult();
+		} catch (NoResultException nre) {
+			entity = null;
+		}
+		if (entity == null) {
+			return null;
+		}
+		float temperature = entity.getLastLoggedTemp() + entity.getOffset();
+		return new TemperatureDTO(entity.getLastLogged(), temperature);
+	}
+
+	protected List<Object[]> executeNativeQuery(String queryString) {
+		Query query = em.createNativeQuery(queryString);
+		@SuppressWarnings("unchecked")
+		List<Object[]> resultList = query.getResultList();
+		return resultList;
+
+	}
+}
--- src/main/java/se/anviken/owmanager/rest/DataEndpoint.java
+++ src/main/java/se/anviken/owmanager/rest/DataEndpoint.java
@@ -1,22 +1,9 @@
 package se.anviken.owmanager.rest;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
 
 import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -31,25 +18,21 @@ import com.google.visualization.datasource.datatable.TableRow;
 import com.google.visualization.datasource.datatable.value.ValueType;
 import com.google.visualization.datasource.render.JsonRenderer;
 
-import se.anviken.owmanager.dto.TemperatureDTO;
-import se.anviken.owmanager.model.Sensor;
 import se.anviken.owmanager.model.Temperature;
+import se.anviken.owmanager.persist.PersistenceHelper;
 import se.anviken.owmanager.utils.DataUtil;
-import se.anviken.owmanager.utils.TimeUtil;
 
 /**
  * 
  */
 @Stateless
 @Path("/data")
-public class DataEndpoint {
+public class DataEndpoint extends PersistenceHelper {
 	private static final String YEARS = "years";
 	private static final String MONTHS = "months";
 	private static final String WEEKS = "weeks";
 	private static final String DAYS = "days";
 	private static final int DEFAULT_NO_OF_MINUTES = 60;
-	@PersistenceContext(unitName = "OWManager-persistence-unit")
-	private EntityManager em;
 
 	@GET
 	@Path("/")
@@ -86,132 +69,6 @@ public class DataEndpoint {
 	private List<Temperature> getDataSet(int id) {
 		return getDataSet(id, DEFAULT_NO_OF_MINUTES);
 	}
-
-	private List<Temperature> getDataSet(int id, int noofminutes) {
-		Calendar from = Calendar.getInstance();
-		Calendar to = Calendar.getInstance();
-		from.add(Calendar.MINUTE, -noofminutes);
-		TypedQuery<Temperature> tempQuery = em.createQuery(
-				"SELECT DISTINCT t FROM Temperature t LEFT JOIN FETCH t.sensor WHERE t.sensor.sensorId = :id AND t.tempTimestamp BETWEEN :from AND :to",
-				Temperature.class);
-		tempQuery.setParameter("from", from.getTime());
-		tempQuery.setParameter("to", to.getTime());
-		tempQuery.setParameter("id", id);
-		return tempQuery.getResultList();
-	}
-
-	private Response getDatatable(String ids, int noofminutes) {
-		String[] strings = ids.split("\\s*,\\s*");
-		Integer[] intarray = new Integer[strings.length];
-		int i = 0;
-		for (String str : strings) {
-			intarray[i] = Integer.parseInt(str.trim());
-			i++;
-		}
-
-		List<Integer> sensorIDs = Arrays.asList(intarray);
-		TypedQuery<Sensor> query = em.createQuery("SELECT s FROM Sensor s where s.sensorId IN :sensors", Sensor.class);
-		query.setParameter("sensors", sensorIDs);
-		final List<Sensor> results = query.getResultList();
-		DataTable data = new DataTable();
-		ArrayList<ColumnDescription> cd = new ArrayList<ColumnDescription>();
-		cd.add(new ColumnDescription("Tid", ValueType.TEXT, "Tid"));
-		for (Sensor sensor : results) {
-			cd.add(new ColumnDescription(sensor.getName(), ValueType.NUMBER, sensor.getName()));
-		}
-		data.addColumns(cd);
-		List<Map<Date, Float>> temperatureLists = new ArrayList<Map<Date, Float>>();
-		Set<Date> timestamps = new HashSet<Date>();
-
-		for (int id : sensorIDs) {
-			Map<Date, Float> temperarures = new HashMap<Date, Float>();
-			for (Temperature temperature : getDataSet(id, noofminutes)) {
-				temperarures.put(temperature.getTempTimestamp(), temperature.getTemperature());
-				timestamps.add(temperature.getTempTimestamp());
-			}
-			temperatureLists.add(temperarures);
-		}
-		timestamps = new TreeSet<Date>(timestamps);
-		for (Date timestamp : timestamps) {
-			TableRow row = new TableRow();
-			row.addCell(TimeUtil.getTimeOfDay(timestamp));
-			for (Map<Date, Float> temperatureList : temperatureLists) {
-				row.addCell(temperatureList.get(timestamp));
-			}
-			try {
-				data.addRow(row);
-			} catch (TypeMismatchException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		return Response.ok(JsonRenderer.renderDataTable(data, true, false, true).toString(), MediaType.APPLICATION_JSON)
-				.build();
-	}
-
-	private Response getDatatableByType(String type, int noofminutes) {
-		String[] typeArray = type.split("\\s*,\\s*");
-		List<String> sensorTypes = Arrays.asList(typeArray);
-		TypedQuery<Sensor> query = em
-				.createQuery("SELECT s FROM Sensor s where s.sensorType.sensorType IN :sensorTypes", Sensor.class);
-		query.setParameter("sensorTypes", sensorTypes);
-		final List<Sensor> results = query.getResultList();
-		DataTable data = new DataTable();
-		ArrayList<ColumnDescription> cd = new ArrayList<ColumnDescription>();
-		cd.add(new ColumnDescription("Tid", ValueType.TEXT, "Tid"));
-
-		List<Map<Date, Float>> temperatureLists = new ArrayList<Map<Date, Float>>();
-		Set<Date> timestamps = new HashSet<Date>();
-
-		for (Sensor sensor : results) {
-			cd.add(new ColumnDescription(sensor.getName(), ValueType.NUMBER, sensor.getName()));
-			Map<Date, Float> temperarures = new HashMap<Date, Float>();
-			for (Temperature temperature : getDataSet(sensor.getSensorId(), noofminutes)) {
-				temperarures.put(temperature.getTempTimestamp(), temperature.getTemperature());
-				timestamps.add(temperature.getTempTimestamp());
-			}
-			temperatureLists.add(temperarures);
-		}
-		data.addColumns(cd);
-		timestamps = new TreeSet<Date>(timestamps);
-		for (Date timestamp : timestamps) {
-			TableRow row = new TableRow();
-			row.addCell(TimeUtil.getTimeOfDay(timestamp));
-			for (Map<Date, Float> temperatureList : temperatureLists) {
-				row.addCell(temperatureList.get(timestamp));
-			}
-			try {
-				data.addRow(row);
-			} catch (TypeMismatchException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		return Response.ok(JsonRenderer.renderDataTable(data, true, false, true).toString(), MediaType.APPLICATION_JSON)
-				.build();
-	}
-	/*
-	 * private Response getHPDatatable() {
-	 * 
-	 * List<Integer> sensorIDs = Arrays.asList(intarray); TypedQuery<Sensor>
-	 * query =
-	 * em.createQuery("SELECT s FROM Sensor s where s.sensorId IN :sensors",
-	 * Sensor.class); query.setParameter("sensors", sensorIDs); final
-	 * List<Sensor> results = query.getResultList(); DataTable data = new
-	 * DataTable(); ArrayList<ColumnDescription> cd = new
-	 * ArrayList<ColumnDescription>(); cd.add(new ColumnDescription("Tid",
-	 * ValueType.TEXT, "Tid")); cd.add(new ColumnDescription("Antal starter",
-	 * ValueType.NUMBER, "Antal starter")); data.addColumns(cd); List<Map<Date,
-	 * Float>> temperatureLists = new ArrayList<Map<Date, Float>>(); Set<Date>
-	 * timestamps = new HashSet<Date>();
-	 * 
-	 * for (Date timestamp : timestamps) { TableRow row = new TableRow();
-	 * row.addCell(TimeUtil.getTimeOfDay(timestamp));
-	 * row.addCell(temperatureList.get(timestamp)); data.addRow(row);
-	 * 
-	 * } return Response.ok(JsonRenderer.renderDataTable(data, true, false,
-	 * true).toString(), MediaType.APPLICATION_JSON) .build(); }
-	 */
 
 	@GET
 	@Path("/getdatatable/{ids}")
@@ -252,10 +109,7 @@ public class DataEndpoint {
 				|| type.equalsIgnoreCase(YEARS)) {
 			String queryString = getMinAvgMaxQueryString(type, id);
 
-			Query query = em.createNativeQuery(queryString);
-
-			@SuppressWarnings("unchecked")
-			List<Object[]> resultList = query.getResultList();
+			List<Object[]> resultList = executeNativeQuery(queryString);
 
 			DataTable data = new DataTable();
 			ArrayList<ColumnDescription> cd = new ArrayList<ColumnDescription>();
@@ -378,24 +232,6 @@ public class DataEndpoint {
 			@PathParam("range") int range, @PathParam("minpeakvalue") int minpeakvalue,
 			@PathParam("trimrange") int trimrange) {
 		return DataUtil.FindPeaksInDataset(getDataSet(id, noofminutes), range, minpeakvalue, trimrange).size();
-	}
-
-	private TemperatureDTO getTemperature(int id) {
-		TypedQuery<Sensor> findByIdQuery = em.createQuery(
-				"SELECT DISTINCT s FROM Sensor s LEFT JOIN FETCH s.sensorType WHERE s.sensorId = :entityId ORDER BY s.sensorId",
-				Sensor.class);
-		findByIdQuery.setParameter("entityId", id);
-		Sensor entity;
-		try {
-			entity = findByIdQuery.getSingleResult();
-		} catch (NoResultException nre) {
-			entity = null;
-		}
-		if (entity == null) {
-			return null;
-		}
-		float temperature = entity.getLastLoggedTemp() + entity.getOffset();
-		return new TemperatureDTO(entity.getLastLogged(), temperature);
 	}
 
 	@GET
